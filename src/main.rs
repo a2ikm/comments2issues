97203a19320d1a2f265fd1f230505010b96da7bf
Masato Ikeda <masato.ikeda@gmail.com>
2024-02-03 00:35:45 +0900

    Implement GET comments

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,54 @@
 use std::{env, process};
 
 fn main() {
-    let token = env::var("GITHUB_TOKEN").unwrap_or_else(|_| {
+    let token = env::var("GITHUB_TOKEN").unwrap_or_else(|_e| {
         eprintln!("GITHUB_TOKEN environment variable is not set");
         process::exit(1);
     });
 
-    println!("token = {token}", token = token);
+    let mut headers = reqwest::header::HeaderMap::new();
+    headers.insert(
+        "Accept",
+        reqwest::header::HeaderValue::from_str("application/vnd.github+json").unwrap(),
+    );
+    headers.insert(
+        "Authorization",
+        reqwest::header::HeaderValue::from_str(format!("Bearer {token}", token = token).as_str())
+            .unwrap(),
+    );
+    headers.insert(
+        "X-GitHub-Api-Version",
+        reqwest::header::HeaderValue::from_str("2022-11-28").unwrap(),
+    );
+
+    let client = reqwest::blocking::ClientBuilder::new()
+        .user_agent(concat!(
+            env!("CARGO_PKG_NAME"),
+            "/",
+            env!("CARGO_PKG_VERSION")
+        ))
+        .default_headers(headers)
+        .build()
+        .unwrap_or_else(|_e| {
+            eprintln!("Failed to build client");
+            process::exit(1);
+        });
+
+    let comments_url = reqwest::Url::parse(
+        format!(
+            "https://api.github.com/repos/{owner}/{repo}/issues/{issue_number}/comments",
+            owner = "a2ikm",
+            repo = "comments2issues",
+            issue_number = "1"
+        )
+        .as_str(),
+    )
+    .unwrap();
+
+    let response = client.get(comments_url).send().unwrap_or_else(|_e| {
+        eprintln!("Failed to get the issue");
+        process::exit(1);
+    });
+
+    println!("{}", response.text().unwrap());
 }
