6647f9d32a0fa0bc670165f9e3637443a26e252b
Masato Ikeda <masato.ikeda@gmail.com>
2024-02-04 03:44:33 +0900

    Extract HTTP requests to client module

---
+++ src/client.rs
@@ -0,0 +1,109 @@
+use std::{env, process};
+
+pub struct Client {
+    owner: String,
+    repo: String,
+    issue_number: String,
+    client: reqwest::blocking::Client,
+}
+
+#[derive(serde::Deserialize)]
+pub struct Comment {
+    pub body: String,
+}
+
+#[derive(serde::Serialize)]
+pub struct Issue {
+    pub title: String,
+    pub body: String,
+}
+
+impl Client {
+    pub fn new(token: &str) -> Client {
+        let mut headers = reqwest::header::HeaderMap::new();
+        headers.insert(
+            "Accept",
+            reqwest::header::HeaderValue::from_str("application/vnd.github+json").unwrap(),
+        );
+        headers.insert(
+            "Authorization",
+            reqwest::header::HeaderValue::from_str(
+                format!("Bearer {token}", token = token).as_str(),
+            )
+            .unwrap(),
+        );
+        headers.insert(
+            "X-GitHub-Api-Version",
+            reqwest::header::HeaderValue::from_str("2022-11-28").unwrap(),
+        );
+
+        let client = reqwest::blocking::ClientBuilder::new()
+            .user_agent(concat!(
+                env!("CARGO_PKG_NAME"),
+                "/",
+                env!("CARGO_PKG_VERSION")
+            ))
+            .default_headers(headers)
+            .build()
+            .unwrap_or_else(|_e| {
+                eprintln!("Failed to build client");
+                process::exit(1);
+            });
+
+        Client {
+            owner: String::from("a2ikm"),
+            repo: String::from("comments2issues"),
+            issue_number: String::from("2"), // https://github.com/a2ikm/comments2issues/issues/2
+            client: client,
+        }
+    }
+
+    pub fn get_comments(&self) -> Vec<Comment> {
+        let url = reqwest::Url::parse(&self.comments_api_url()).unwrap();
+        let response = self.client.get(url).send().unwrap_or_else(|_e| {
+            eprintln!("Failed to get the issue");
+            process::exit(1);
+        });
+
+        let json = response.text().unwrap();
+        let comments: Vec<Comment> = serde_json::from_str(&json).unwrap_or_else(|_e| {
+            eprintln!("Failed to parse JSON response");
+            process::exit(1);
+        });
+
+        comments
+    }
+
+    pub fn create_issue(&self, issue: Issue) {
+        let url = reqwest::Url::parse(&self.issues_api_url()).unwrap();
+
+        let response = self
+            .client
+            .post(url)
+            .json(&issue)
+            .send()
+            .unwrap_or_else(|_e| {
+                eprintln!("Failed to create a new issue");
+                process::exit(1);
+            });
+
+        println!("{}", response.status().as_str());
+    }
+
+    fn comments_api_url(&self) -> String {
+        format!(
+            "https://api.github.com/repos/{owner}/{repo}/issues/{issue_number}/comments",
+            owner = self.owner,
+            repo = self.repo,
+            issue_number = self.issue_number,
+        )
+    }
+
+    fn issues_api_url(&self) -> String {
+        format!(
+            "https://api.github.com/repos/{owner}/{repo}/issues",
+            owner = self.owner,
+            repo = self.repo,
+        )
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,6 @@
+mod client;
+
 use std::{env, process};
-
-#[derive(serde::Deserialize)]
-struct Comment {
-    body: String,
-}
-
-#[derive(serde::Serialize)]
-struct Issue {
-    title: String,
-    body: String,
-}
 
 fn main() {
     let token = env::var("GITHUB_TOKEN").unwrap_or_else(|_e| {
@@ -17,88 +8,20 @@ fn main() {
         process::exit(1);
     });
 
-    let mut headers = reqwest::header::HeaderMap::new();
-    headers.insert(
-        "Accept",
-        reqwest::header::HeaderValue::from_str("application/vnd.github+json").unwrap(),
-    );
-    headers.insert(
-        "Authorization",
-        reqwest::header::HeaderValue::from_str(format!("Bearer {token}", token = token).as_str())
-            .unwrap(),
-    );
-    headers.insert(
-        "X-GitHub-Api-Version",
-        reqwest::header::HeaderValue::from_str("2022-11-28").unwrap(),
-    );
+    let client = client::Client::new(&token);
 
-    let client = reqwest::blocking::ClientBuilder::new()
-        .user_agent(concat!(
-            env!("CARGO_PKG_NAME"),
-            "/",
-            env!("CARGO_PKG_VERSION")
-        ))
-        .default_headers(headers)
-        .build()
-        .unwrap_or_else(|_e| {
-            eprintln!("Failed to build client");
-            process::exit(1);
-        });
-
-    let comments_url = reqwest::Url::parse(
-        format!(
-            "https://api.github.com/repos/{owner}/{repo}/issues/{issue_number}/comments",
-            owner = "a2ikm",
-            repo = "comments2issues",
-            issue_number = "2" // https://github.com/a2ikm/comments2issues/issues/2
-        )
-        .as_str(),
-    )
-    .unwrap();
-
-    let response = client.get(comments_url).send().unwrap_or_else(|_e| {
-        eprintln!("Failed to get the issue");
-        process::exit(1);
-    });
-
-    let mut comments: Vec<Comment> = serde_json::from_str(response.text().unwrap().as_str())
-        .unwrap_or_else(|_e| {
-            eprintln!("Failed to parse JSON response");
-            process::exit(1);
-        });
+    let mut comments = client.get_comments();
 
     println!("number of comments: {}", comments.len());
 
     for comment in comments.iter_mut() {
         println!("body = {}", comment.body);
 
-        let issues_url = reqwest::Url::parse(
-            format!(
-                "https://api.github.com/repos/{owner}/{repo}/issues",
-                owner = "a2ikm",
-                repo = "comments2issues",
-            )
-            .as_str(),
-        )
-        .unwrap_or_else(|_e| {
-            eprintln!("Failed to parse issues url");
-            process::exit(1);
-        });
-
-        let issue = Issue {
+        let issue = client::Issue {
             title: String::from("Created by comments2issues"),
             body: comment.body.clone(),
         };
 
-        let response = client
-            .post(issues_url)
-            .json(&issue)
-            .send()
-            .unwrap_or_else(|_e| {
-                eprintln!("Failed to create a new issue");
-                process::exit(1);
-            });
-
-        println!("{}", response.status().as_str());
+        client.create_issue(issue);
     }
 }
