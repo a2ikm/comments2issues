9993f392d60f9c92596f66fa8c5aa7200aa624db
Masato Ikeda <masato.ikeda@gmail.com>
2024-02-04 03:03:43 +0900

    Create issues from comments

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,12 @@ struct Comment {
     body: String,
 }
 
+#[derive(serde::Serialize)]
+struct Issue {
+    title: String,
+    body: String,
+}
+
 fn main() {
     let token = env::var("GITHUB_TOKEN").unwrap_or_else(|_e| {
         eprintln!("GITHUB_TOKEN environment variable is not set");
@@ -44,7 +50,7 @@ fn main() {
             "https://api.github.com/repos/{owner}/{repo}/issues/{issue_number}/comments",
             owner = "a2ikm",
             repo = "comments2issues",
-            issue_number = "1"
+            issue_number = "2" // https://github.com/a2ikm/comments2issues/issues/2
         )
         .as_str(),
     )
@@ -61,7 +67,38 @@ fn main() {
             process::exit(1);
         });
 
+    println!("number of comments: {}", comments.len());
+
     for comment in comments.iter_mut() {
-        println!("{}", comment.body);
+        println!("body = {}", comment.body);
+
+        let issues_url = reqwest::Url::parse(
+            format!(
+                "https://api.github.com/repos/{owner}/{repo}/issues",
+                owner = "a2ikm",
+                repo = "comments2issues",
+            )
+            .as_str(),
+        )
+        .unwrap_or_else(|_e| {
+            eprintln!("Failed to parse issues url");
+            process::exit(1);
+        });
+
+        let issue = Issue {
+            title: String::from("Created by comments2issues"),
+            body: comment.body.clone(),
+        };
+
+        let response = client
+            .post(issues_url)
+            .json(&issue)
+            .send()
+            .unwrap_or_else(|_e| {
+                eprintln!("Failed to create a new issue");
+                process::exit(1);
+            });
+
+        println!("{}", response.status().as_str());
     }
 }
